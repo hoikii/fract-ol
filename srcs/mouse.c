037761c46848661d1541417b34fc1faccc55d998
kanlee <kanlee@student.42seoul.kr>
2021-06-02 17:17:11 +0900

    mouse wheel to zoom

--- srcs/mouse.c
+++ srcs/mouse.c
@@ -6,16 +6,20 @@
 /*   By: kanlee <kanlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 12:51:17 by kanlee            #+#    #+#             */
-/*   Updated: 2021/05/30 16:47:24 by kanlee           ###   ########.fr       */
+/*   Updated: 2021/06/02 17:16:23 by kanlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "mouse.h"
 
+
 int	mouse_hook(int btn, int click_x, int click_y, t_mlx *frame)
 {
-	if (btn == 1)
+printf("btn=%d, x=%d, y=%d\n", btn, click_x, click_y);
+	if (click_y <= 0)
+		return (0);
+	if (btn == MOUSE_LCLICK)
 	{
 		frame->center.x = frame->upperleft.x + click_x / frame->scale;
 		frame->center.y = frame->upperleft.y - click_y / frame->scale;
@@ -23,8 +27,20 @@ int	mouse_hook(int btn, int click_x, int click_y, t_mlx *frame)
 	frame->upperleft.y = frame->center.y + frame->height / frame->scale / 2;
 		render(*frame);
 	}
-		printf("btn=%d, x=%d, y=%d\n", btn, click_x, click_y);
-//	if (btn != MOUSE_LCLICK || click_y <= 0)
-//		return (0);
+	else if (btn == MOUSE_WHEELDOWN || btn == MOUSE_WHEELUP)
+	{
+		frame->center.x = frame->upperleft.x + click_x / frame->scale;
+		frame->center.y = frame->upperleft.y - click_y / frame->scale;
+
+		if (btn == MOUSE_WHEELDOWN)
+			frame->scale *= 2;
+		else
+			frame->scale /= 2;
+		frame->upperleft.x = frame->center.x - frame->width / frame->scale / 2;
+		frame->upperleft.y = frame->center.y + frame->height / frame->scale / 2;
+
+		render(*frame);
+	}
+
 	return (0);
 }
