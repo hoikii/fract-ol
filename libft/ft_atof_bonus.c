d1c7844bd310b8946062d20214bdb53ab9c34633
kanlee <kanlee@student.42seoul.kr>
2021-11-23 14:56:52 +0900

    fix atof when input is between -1..0

--- libft/ft_atof_bonus.c
+++ libft/ft_atof_bonus.c
@@ -6,11 +6,25 @@
 /*   By: kanlee <kanlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 17:16:08 by kanlee            #+#    #+#             */
-/*   Updated: 2021/06/05 01:47:22 by kanlee           ###   ########.fr       */
+/*   Updated: 2021/11/23 14:51:03 by kanlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	chk_sign(const char *str)
+{
+	int	sign;
+
+	sign = 1;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	return (sign);
+}
 
 double	ft_atof(const char *str)
 {
@@ -19,12 +33,9 @@ double	ft_atof(const char *str)
 	double	decimal;
 
 	ret = ft_atoi(str);
-	sign = 1;
+	sign = chk_sign(str);
 	if (ret < 0)
-	{
-		sign = -1;
 		ret *= -1;
-	}
 	while (*str == '+' || *str == '-' || ft_isdigit(*str))
 		str++;
 	decimal = 1.0;
